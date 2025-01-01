6683f6df15001ff57388476a4de9c09e2fd28e95
Andre Sadighi <andresadighi@protonmail.com>
2025-01-01 14:13:04 -0800

    Fixed bad logic. Previously the timestamps were the ending time. Now they are the starting time.
    Fixed the end of timestamp bug. (It wasn't really a bug, it was just an incomplete script...)

--- yt-chopper.py
+++ yt-chopper.py
@@ -1,6 +1,7 @@
 import os 
 import string
 import pathlib
+import subprocess
 
 ####
 # Directions:
@@ -12,15 +13,15 @@ import pathlib
 ### videofile.mp4
 ### videofile.txt
 ##
-# 0:20; 1_outputTitle.mp4; Metadata Title; 1
+# 0:00; 1_outputTitle.mp4; Metadata Title; 1
 # 0:30; 2_outputTitle.mp4; Second Metadata Title; 2
 ##
-# This will make a video clip from 0:00 to 0:20 (named "1_outputTitle.mp4")
-# and another for 0:20 to 0:30 (named "2_outputTitle.mp4").
+# This will make a video clip from 0:00 to 0:30 (named "1_outputTitle.mp4")
+# and another for 0:30 to the end of the video (named "2_outputTitle.mp4").
 ###
 # Not all options are necessary! You can include as little as timestamps!
 ##
-# 0:20
+# 0:00
 # 0:30
 ##
 # This would make the same clips but autogenerate a filename!
@@ -48,6 +49,19 @@ def getVideoQueue():
 
     return videoqueue
 
+def getVideoLength(filename):
+    command = [
+        'ffprobe', '-v', 'error', '-show_entries', 'format=duration',
+        '-of', 'default=noprint_wrappers=1:nokey=1', '-sexagesimal', filename
+    ]
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+        return None
+
+    return result.stdout.strip()
+
 startTime = "00:00"
 stopTime = "00:00"
 
@@ -55,16 +69,24 @@ videoQueue = getVideoQueue()
 
 for videoFilename in videoQueue:
     timestamps = getTimestamps(os.path.splitext(videoFilename)[0] + ".txt")
+    videoLength = getVideoLength(videoFilename)
     
     timestampCounter = 0
     timestampLength = len(timestamps)
 
     while timestampCounter < timestampLength:
        currentTimestamp = timestamps[timestampCounter].split(";")
-       # 1:00; filename.mp4; File Title; 1
-       # Stop; filename.mp4; File Title; File Track #
+       try: 
+        nextTimestamp = timestamps[timestampCounter+1].split(";")
+        stopTime = nextTimestamp[0].strip()
+       except:
+        stopTime = videoLength
 
-       stopTime = currentTimestamp[0].strip()
+
+       # 1:00; filename.mp4; File Title; 1
+       # Start; filename.mp4; File Title; File Track #
+
+       startTime = currentTimestamp[0].strip()
 
        # Temporary name incase no input.
        outputFilename = f"clip_{startTime.replace(':', '-')}_{stopTime.replace(':', '-')}_{videoFilename[:-4]}.mp4"
